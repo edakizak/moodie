bf2f545d1ca1fedac626dd443f9d8dcddd748668
edakizak <edakizak.de@gmail.com>
2024-02-20 14:07:44 +0100

    test vercel

--- components/Layout/Layout.js
+++ components/Layout/Layout.js
@@ -2,21 +2,21 @@
 import Navigation from "../Navigation/Navigation";
 import styled from "styled-components";
 
-const Main = styled.main`
-  display: grid;
-  gap: 0.5rem;
-  margin-top: 5rem;
-  padding: 0.5rem;
-  position: relative;
-  width: 100%;
-  justify-content: center;
-`;
+// const Main = styled.main`
+//   display: grid;
+//   gap: 0.5rem;
+//   margin-top: 5rem;
+//   padding: 0.5rem;
+//   position: relative;
+//   width: 100%;
+//   justify-content: center;
+// `;
 
 export default function Layout({ children }) {
   return (
     <>
       <Navigation />
-      <Main>{children}</Main>
+      {/* <Main>{children}</Main> */}
     </>
   );
 }
--- components/SearchBox/SearchBox.js
+++ components/SearchBox/SearchBox.js
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
 // import AutoComplete from "./AutoComplete";
+import styles from "./SearchBox.module.css";
+import { CiSearch } from "react-icons/ci";
 
 export default function SearchBox({ onSearch }) {
   const [searchTerm, setSearchTerm] = useState("");
@@ -10,14 +12,17 @@ export default function SearchBox({ onSearch }) {
   };
 
   return (
-    <form onSubmit={handleSearch}>
+    <form onSubmit={handleSearch} className={styles.form}>
       <input
+        className={styles.input}
         type="text"
         placeholder="Search for a movie..."
         value={searchTerm}
         onChange={(e) => setSearchTerm(e.target.value)}
       />
-      <button type="submit">Search</button>
+      <button className={styles.button} type="submit">
+        <CiSearch />
+      </button>
     </form>
   );
 }
