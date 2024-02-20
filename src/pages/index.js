c894efc7d75ab3960f638189c65c2a25064193a1
edakizak <edakizak.de@gmail.com>
2024-02-20 19:01:49 +0100

    Add moviedetails

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,7 @@ import { useState, useEffect } from "react";
 import SearchBox from "../../components/SearchBox/SearchBox";
 import Card from "../../components/Card/Card";
 import MovieDetails from "../../components/MovieDetails/MovieDetails";
+import styles from "../../components/Card/Card.module.css";
 
 export default function Home() {
   const [movies, setMovies] = useState([]);
@@ -28,14 +29,35 @@ export default function Home() {
   return (
     <div>
       <SearchBox onSearch={searchMovies} />
-      <ul>
-        {movies.map((movie) => (
-          <div key={movie.id} onClick={() => fetchMovieDetails(movie.id)}>
-            <Card key={movie.id} movie={movie} />
-          </div>
-        ))}
-      </ul>
-      {selectedMovieDetails && <MovieDetails movie={selectedMovieDetails} />}
+      <div style={{ display: "flex", marginTop: "20px" }}>
+        <ul
+          style={{
+            display: "flex",
+            flexWrap: "wrap",
+            padding: 0,
+            margin: "0 20px 0 0",
+            maxWidth: "70%",
+            gap: "20px",
+            overflowY: "auto",
+          }}
+        >
+          {movies.map((movie) => (
+            <li
+              key={movie.id}
+              className={styles.cardContainer}
+              onClick={() => fetchMovieDetails(movie.id)}
+              style={{ cursor: "pointer" }}
+            >
+              <Card key={movie.id} movie={movie} />
+            </li>
+          ))}
+        </ul>
+        <div style={{ flex: 1 }}>
+          {selectedMovieDetails && (
+            <MovieDetails movie={selectedMovieDetails} />
+          )}
+        </div>
+      </div>
     </div>
   );
 }
