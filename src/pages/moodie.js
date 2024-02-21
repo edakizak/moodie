a62b96cda34a001a68c304ceaae9cdc3df12f966
edakizak <edakizak.de@gmail.com>
2024-02-21 16:32:40 +0100

    add moodie page

---
+++ src/pages/moodie.js
@@ -0,0 +1,10 @@
+import OpenAiSearchBox from "../../components/OpenAiSearchBox/OpenAiSearchBox";
+
+export default function Moodie() {
+  return (
+    <div>
+      <h1>What is your mood?</h1>
+      <OpenAiSearchBox />
+    </div>
+  );
+}
