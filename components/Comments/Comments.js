e7ce9b6d11c0711aa24dbc7b264ab0c6e4d4c959
edakizak <edakizak.de@gmail.com>
2024-02-22 12:58:55 +0100

    db connect- schema

--- components/Comments/Comments.js
+++ components/Comments/Comments.js
@@ -1,40 +1,40 @@
-// import styles from "./Comments.module.css";
-
-// export default function Comments({ name, content, timestamp }) {
-//   return (
-//     <div className={styles.container}>
-//       <div className={styles.name}>{name}</div>
-//       <div className={styles.content}>{content}</div>
-//       <div className={styles.timestamp}>{timestamp}</div>
-//     </div>
-//   );
-// }
-
 import styles from "./Comments.module.css";
 
-// Comments bileşeni tüm yorumları listelemek için
-export default function Comments({ comments }) {
-  // Comment bileşeni individual yorumlar için
-  const Comment = ({ name, content, timestamp }) => {
-    return (
-      <div className={styles.container}>
-        <div className={styles.name}>{name}</div>
-        <div className={styles.content}>{content}</div>
-        <div className={styles.timestamp}>{timestamp}</div>
-      </div>
-    );
-  };
-
+export default function Comments({ name, content, timestamp }) {
   return (
-    <div>
-      {comments.map((comment) => (
-        <Comment
-          key={comment._id}
-          name={comment.name}
-          content={comment.comment}
-          timestamp={comment.timestamp}
-        />
-      ))}
+    <div className={styles.container}>
+      <div className={styles.name}>{name}</div>
+      <div className={styles.content}>{content}</div>
+      <div className={styles.timestamp}>{timestamp}</div>
     </div>
   );
 }
+
+// import styles from "./Comments.module.css";
+
+// // Comments bileşeni tüm yorumları listelemek için
+// export default function Comments({ comments }) {
+//   // Comment bileşeni individual yorumlar için
+//   const Comment = ({ name, content, timestamp }) => {
+//     return (
+//       <div className={styles.container}>
+//         <div className={styles.name}>{name}</div>
+//         <div className={styles.content}>{content}</div>
+//         <div className={styles.timestamp}>{timestamp}</div>
+//       </div>
+//     );
+//   };
+
+//   return (
+//     <div>
+//       {comments.map((comment) => (
+//         <Comment
+//           key={comment._id}
+//           name={comment.name}
+//           content={comment.comment}
+//           timestamp={comment.timestamp}
+//         />
+//       ))}
+//     </div>
+//   );
+// }
